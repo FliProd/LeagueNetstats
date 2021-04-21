ac688cc1382a4b9876591ec3e317bc6902ac76e8
Noah Hampp <noahhampp@student-net-cx-2078.ethz.ch>
2021-04-21 11:03:23 +0200

    no-ssl version

--- LeagueStats/LeagueStats/settings.py
+++ LeagueStats/LeagueStats/settings.py
@@ -235,3 +235,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+#SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
--- LeagueStats/entrypoint.prod.sh
+++ LeagueStats/entrypoint.prod.sh
@@ -3,6 +3,10 @@
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
+    echo $SQL_HOST
+    echo $SQL_PORT
+    echo $(nc -z $SQL_HOST $SQL_PORT)
+    echo $( ! nc -z $SQL_HOST $SQL_PORT)
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
       sleep 0.1
